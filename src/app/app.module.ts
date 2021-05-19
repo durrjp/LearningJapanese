a7cfcbe5d2e70e080a8ff8b4385777a5fd1f9139
Jon <durrjp1@gmail.com>
2021-05-19 18:17:48 -0500

    basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlashCardsComponent } from './components/flash-cards/flash-cards.component';
+import { GuessingGameComponent } from './components/guessing-game/guessing-game.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlashCardsComponent,
+    GuessingGameComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/guessing-game/guessing-game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-guessing-game',
+  templateUrl: './guessing-game.component.html',
+  styleUrls: ['./guessing-game.component.css']
+})
+export class GuessingGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
